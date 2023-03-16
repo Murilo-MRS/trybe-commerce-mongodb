d5e3786fd71bab647af3be96905fa03eaf5ee637
Murilo-MRS <murilosantana1993@gmail.com>
2023-03-16 02:09:04 -0300

    fix: req-20

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,8 +1,5 @@
 db.produtos.updateOne(
   { nome: "Quarteirão com Queijo" },
-  { $pop: {
-      ingredientes: -1,
-    },
-  },
+  { $pop: { ingredientes: -1 } },
 );
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
